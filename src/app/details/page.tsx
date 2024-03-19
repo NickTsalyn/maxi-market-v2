98e1fd28cc6ac2b060f0fef611d085515c0d5b28
NickTsalyn <129570585+NickTsalyn@users.noreply.github.com>
2024-03-19 19:41:10 -0400

    add CardSeller and CardDescription

--- src/app/details/page.tsx
+++ src/app/details/page.tsx
@@ -1,7 +1,14 @@
+import CardProductDescription from "@/components/Cards/CardProductDescription";
+import CardSeller from "@/components/Cards/CardSeller";
 import React from "react";
 
 export interface PageProps {}
 
 export default function Page({}: PageProps) {
-  return <div>DetailsAdvert</div>;
+	return (
+		<div>
+			<CardProductDescription />
+      <CardSeller/>
+		</div>
+	);
 }
--- src/components/Cards/CardProductDescription.tsx
+++ src/components/Cards/CardProductDescription.tsx
@@ -1,5 +1,22 @@
+import { IconFavouriteBlack } from "../Icons/Icons";
+import Button from "../UI/Button";
+
 type Props = {};
 
 export default function CardProductDescription(props: Props) {
-  return <div>CardProductDescription</div>;
+	return (
+		<div className=" flex flex-col gap-4 md:gap-6">
+			<div className="flex justify-between items-center">
+				<h3 className=" text-base font-medium xl:text-lg xl:font-semibold">Рюкзак &quot;Експлорер&quot;</h3>
+				<Button style="favourite" type="button">
+					<IconFavouriteBlack/>
+				</Button>
+			</div>
+			<span className="body-2">опубліковано вчора о 13:43</span>
+			<h3 className="header-3">
+				1500 <span className="text-base ">грн</span>
+			</h3>
+			<Button style="wide-primary" type="submit">Купити</Button>
+		</div>
+	);
 }
