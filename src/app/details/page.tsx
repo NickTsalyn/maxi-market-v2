9fa692f3376a9fa3fc85d4848bac422e82300ab2
YuliaSilk <yuliashevchyk0506@gmail.com>
2024-03-29 23:43:18 +0200

    create swiper component

--- src/app/details/page.tsx
+++ src/app/details/page.tsx
@@ -1,7 +1,10 @@
+import SwiperAdwert from "@/components/Swiper/SwiperAdwert";
 import React from "react";
 
 export interface PageProps {}
 
 export default function Page({}: PageProps) {
-  return <div>DetailsAdvert</div>;
+  return <div>DetailsAdvert
+  <SwiperAdwert/>
+  </div>;
 }
---
+++ src/app/lib/images.js
@@ -0,0 +1,13 @@
+import main from "../../components/Images/main.jpg";
+import first from "../../components/Images/first.jpg";
+import second from "../../components/Images/second.jpg";
+import third from "../../components/Images/third.jpg";
+import fours from "../../components/Images/fours.jpg";
+
+export const images = [
+  { src: main, alt: "main photo" },
+  { src: first, alt: "photo of advert" },
+  { src: second, alt: "photo of advert" },
+  { src: third, alt: "photo of advert" },
+  { src: fours, alt: "photo of advert" },
+];
